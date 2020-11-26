32705c0f543597273694adc43ad18a8cddfd3599
Jesse <weihongji@qq.com>
2020-11-26 23:30:06 +0800

    First commit

---
+++ add-content.js
@@ -0,0 +1,6 @@
+export default function() {
+	// document.write('Hello world!'); //npx webpack --entry=./index.js --output-filename=bundle.js --mode=development
+	// document.write("I'm using npm scripts!"); //npm run build
+	//document.write("I'm using a config file!"); //"npx webpack" or "npm run build"
+	document.write("I'm using webpack-dev-server!"); //"npx webpack serve" or "npm run dev"
+}
---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	entry: './index.js',
+	output: {
+		filename: 'bundle.js'
+	},
+	mode: 'development',
+	devServer: {
+		publicPath: '/dist'
+	}
+}
